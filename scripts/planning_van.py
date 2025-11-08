48919dc84df78fac9ab627598c5d0eade5a39ffc
mustaphouni04 <elaichounimustapha@gmail.com>
2025-11-08 23:36:16 +0100

    add scriptsv5

--- scripts/planning_van.py
+++ scripts/planning_van.py
@@ -1,6 +1,19 @@
-from OSMPythonTools.api import Api
+import osmnx as ox
+import folium
 
-api = Api()
-way = api.query('way/5887599')
+place_name = "Granada, Spain"
+G = ox.graph_from_place(place_name, network_type="drive")
 
-print("Way is: ", way)
+nodes, edges = ox.graph_to_gdfs(G)
+
+center = [nodes.geometry.y.mean(), nodes.geometry.x.mean()]
+m = folium.Map(location=center, zoom_start=13)
+
+for _, row in edges.iterrows():
+    folium.PolyLine(
+        locations=[(y, x) for x, y in row.geometry.coords],
+        color="blue", weight=2, opacity=0.7
+    ).add_to(m)
+
+m.save("map.html")
+
